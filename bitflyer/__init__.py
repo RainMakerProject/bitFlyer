7c41eb1271ff4867fa146501186731e005294b10
studio3104 <studio3104.com@gmail.com>
2021-12-12 14:43:04 +0900

    Add 'Position' as one of representatives of the package

--- bitflyer/__init__.py
+++ bitflyer/__init__.py
@@ -1,4 +1,4 @@
 from .client import BitFlyer, BitFlyerRealTime
 from .enumerations import Channel, PublicChannel, ProductCode, State, Candlestick, ChartType
 from .requests import ChildOrderRequest
-from .responses import Balance, ChildOrderResponse, Ticker
+from .responses import Balance, ChildOrderResponse, Ticker, Position
